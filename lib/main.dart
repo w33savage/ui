2def6df265e9dc91d82e0e6aabae689e839de03c
pranav.v <pranav.v@mitsogo.com>
2021-07-27 23:25:15 +0530

    feat(bottomNavigationBar): implement bottom app navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -36,70 +36,130 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenWidgetState();
+}
+
+class _HomeScreenWidgetState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
+    getBodyPageWidget(int index) {
+      switch (index) {
+        case 0:
+          return new _AvailableRoomListWidget();
+      }
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Title'),
-        backgroundColor: kBackgroundColor,
-        elevation: 1,
-        leading: IconButton(
-            onPressed: () {},
-            icon: Badge(
-              child: Icon(
-                Icons.email,
-                color: Colors.black,
-              ),
-              badgeColor: Colors.green,
-              position: BadgePosition.topEnd(top: 1, end: -1),
-            )),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.calendar_today,
-                color: Colors.black,
-              )),
-          IconButton(
+        appBar: AppBar(
+          title: Text('Title'),
+          backgroundColor: kBackgroundColor,
+          elevation: 1,
+          leading: IconButton(
               onPressed: () {},
               icon: Badge(
                 child: Icon(
-                  Icons.notifications,
+                  Icons.email,
                   color: Colors.black,
                 ),
                 badgeColor: Colors.green,
-                badgeContent: Text(
-                  '3',
-                  // style: TextStyle(color: Colors.red),
-                ),
+                position: BadgePosition.topEnd(top: 1, end: -1),
               )),
-          AnimatedContainer(
-              duration: Duration(seconds: 2),
-              curve: Curves.easeIn,
-              child: Container(
-                padding: EdgeInsets.all(5),
-                width: 60,
-                height: 50,
-                child: Material(
-                    borderRadius: BorderRadius.circular(50),
-                    child: InkWell(
-                      onTap: () {},
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: CachedNetworkImage(
-                          width: 32,
-                          height: 32,
-                          fit: BoxFit.cover,
-                          imageUrl:
-                              'https://lh3.googleusercontent.com/ogw/ADea4I7Abb9tQqHg2l2zJ-9Ifg4JjZgylFIi8yDJYfxzZA=s32-c-mo',
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.calendar_today,
+                  color: Colors.black,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Badge(
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.black,
+                  ),
+                  badgeColor: Colors.green,
+                  badgeContent: Text(
+                    '3',
+                    // style: TextStyle(color: Colors.red),
+                  ),
+                )),
+            AnimatedContainer(
+                duration: Duration(seconds: 2),
+                curve: Curves.easeIn,
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  width: 60,
+                  height: 50,
+                  child: Material(
+                      borderRadius: BorderRadius.circular(50),
+                      child: InkWell(
+                        onTap: () {},
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: CachedNetworkImage(
+                            width: 32,
+                            height: 32,
+                            fit: BoxFit.cover,
+                            imageUrl:
+                                'https://lh3.googleusercontent.com/ogw/ADea4I7Abb9tQqHg2l2zJ-9Ifg4JjZgylFIi8yDJYfxzZA=s32-c-mo',
+                          ),
                         ),
-                      ),
-                    )),
-              )),
-        ],
+                      )),
+                )),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Room',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contacts),
+              label: 'Contacts',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[900],
+          unselectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
+        body: getBodyPageWidget(_selectedIndex));
+  }
+}
+
+class _AvailableRoomListWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.lightGreen,
+        child: Center(
+          child: Text('Room List'),
+        ),
       ),
     );
   }
