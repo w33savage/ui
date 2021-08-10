2e208d4879a0e856b1617c7f991370d86ee9f0fd
pranav.v <pranav.v@mitsogo.com>
2021-08-10 23:35:24 +0530

    feat(profilepic): add profile pic implementation

--- lib/pages/welcome/pick_photo_page.dart
+++ lib/pages/welcome/pick_photo_page.dart
@@ -1,10 +1,43 @@
+import 'dart:io';
+import 'package:club_house/services/authenticate.dart';
 import 'package:club_house/util/history.dart';
 import 'package:club_house/widgets/round_button.dart';
 import 'package:club_house/util/style.dart';
 import 'package:club_house/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class PickPhotoPage extends StatelessWidget {
+// ref:  https://medium.com/fabcoding/adding-an-image-picker-in-a-flutter-app-pick-images-using-camera-and-gallery-photos-7f016365d856
+class PickPhotoPage extends StatefulWidget {
+  @override
+  _PickPhotoPageState createState() => _PickPhotoPageState();
+}
+
+class _PickPhotoPageState extends State<PickPhotoPage> {
+  final ImagePicker _picker = ImagePicker();
+
+  XFile _image;
+
+  _imgFromCamera() async {
+    // Capture a photo
+    final XFile photo =
+        await _picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = photo;
+    });
+  }
+
+  _imgFromGallery() async {
+    // Pick an image
+    final XFile image =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,18 +101,64 @@ class PickPhotoPage extends StatelessWidget {
 
   Widget buildContents() {
     return Container(
-      width: 200,
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(80),
-      ),
-      child: Icon(
-        Icons.add_photo_alternate_outlined,
-        size: 100,
-        color: Style.AccentBlue,
-      ),
-    );
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(80),
+        ),
+        child: GestureDetector(
+            onTap: () {
+              _showPicker(context);
+            },
+            child: _image != null
+                ? CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(80),
+                      child: Image.file(
+                        File(this._image.path),
+                        width: 160,
+                        height: 160,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ))
+                : Icon(
+                    Icons.add_photo_alternate_outlined,
+                    size: 100,
+                    color: Style.AccentBlue,
+                  )));
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   Widget buildBottom() {
@@ -87,7 +166,16 @@ class PickPhotoPage extends StatelessWidget {
       color: Style.AccentBlue,
       minimumWidth: 230,
       disabledColor: Style.AccentBlue.withOpacity(0.3),
-      onPressed: () {},
+      onPressed: () async {
+        if (this._image != null) {
+          try {
+            await AuthService().uploadProfilePic(this._image);
+            History.pushPageReplacement(context, HomePage());
+          } catch (e) {
+            print(e.message);
+          }
+        }
+      },
       child: Container(
         child: Row(
           mainAxisSize: MainAxisSize.min,
